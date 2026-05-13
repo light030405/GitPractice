d30f7f13e97ed17520a4193e257c6d70c4b5cb1a
light030405 <light030405@gmail.com>
2026-05-13 23:06:10 +0530

    Initial Spring boot setup

---
+++ src/test/java/com/example/gitPractise/GitPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitPractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
